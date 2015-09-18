3d4bd037dbe71ba8d78aacdb31877dff1ba83b7b
Dan Mamaev <dan.mamaev@gmail.com>
2015-09-18 20:43:35 +0300

    Working MergeSort added

--- src/main/java/edu/spbu/cs/IntSort.java
+++ src/main/java/edu/spbu/cs/IntSort.java
@@ -8,9 +8,47 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
-  }
+    private static int other(int id) {
+        return (id == 0 ? 1 : 0);
+    }
+
+    private static boolean inRange(int index, int start, int size, int globalSize) {
+        return index < size && start + index < globalSize;
+    }
+
+    public static void sort(int[] array) {
+        int arr[][] = new int[2][array.length];
+        for (int id = 0; id < 2; ++id) {
+            arr[id] = array;
+        }
+        int id = 0;
+        for (int size = 1; size < array.length; size *= 2, id = other(id)) {
+            for (int first = 0; first < array.length; first += 2 * size) {
+                int second = first + size;
+                int i = 0, j = 0;
+                while (inRange(i, first, size, array.length) && inRange(j, second, size, array.length)) {
+                    if (arr[id][i] < arr[id][j]) {
+                        arr[other(id)][first + i + j] = arr[id][first + i];
+                        ++i;
+                    }
+                    else {
+                        arr[other(id)][first + i + j] = arr[id][second + j];
+                        ++j;
+                    }
+                }
+                while (inRange(i, first, size, array.length)) {
+                    arr[other(id)][first + i + j] = arr[id][first + i];
+                    ++i;
+                }
+                while (inRange(j, second, size, array.length)) {
+                    arr[other(id)][first + i + j] = arr[id][second + j];
+                    ++j;
+                }
+            }
+        }
+        array = arr[id];
+    }
+
 
   public static void sort (List<Integer> list) {
     Collections.sort(list);
